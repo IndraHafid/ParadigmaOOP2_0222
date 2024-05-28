55dec791f688d63e806656c4ece5b04840894ff4
IndraHfid <indrahafidsaputra@gmail.com>
2024-05-28 16:00:44 +0700

    Menambahkan definisi kelas AbstraksiKlas dengan metode set X,Y dan display untuk mengatur dan menampilkan nilai x dan y

--- Abstraksi/Abstraksi.cpp
+++ Abstraksi/Abstraksi.cpp
@@ -2,6 +2,25 @@
 //
 
 #include <iostream>
+using namespace std;
+
+class AbstraksiKlas {
+private: string x, y;
+
+public:
+
+
+
+    void setXY(string a, string b) {
+        x = a;
+        x = b;
+    }
+
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
 
 
 
