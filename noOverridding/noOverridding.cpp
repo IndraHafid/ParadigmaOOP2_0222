9edee7689944884cde9181d5a129e3dd55535507
IndraHfid <indrahafidsaputra@gmail.com>
2024-05-28 15:48:54 +0700

    Menambahkan kata kunci final ke fungsi perkenalan dalam kelas dasar

--- noOverridding/noOverridding.cpp
+++ noOverridding/noOverridding.cpp
@@ -2,6 +2,15 @@
 //
 
 #include <iostream>
+using namespace std;
+
+class baseClass {
+public:
+    virtual void perkenalan() final {
+        cout << "Hallo saya function dari base class";
+    }
+};
+
 
 int main()
 {
